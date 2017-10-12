71b41d793ce474745b447d6adbefea76de32db03
cedric lamoriniere <cedric.lamoriniere@amadeus.com>
2017-10-12 11:36:49 +0200

    release {{.Version}}

--- scripts/helm-release.sh
+++ scripts/helm-release.sh
@@ -3,7 +3,7 @@
 helm package --version "$1" charts/helm-test
 mv "helm-test-$1.tgz" docs/
 helm repo index docs --url https://cedriclam.github.io/helm-test/
-git add -i
+git add --all .
 git commit -am "release $1"
 git tag -f $1
 git push  --tags origin master
