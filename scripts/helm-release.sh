31f24ef6d6ba1b8ec5467eebd8d2edeaad1046bb
cedric lamoriniere <cedric.lamoriniere@amadeus.com>
2017-10-12 11:44:20 +0200

    release v0.0.5

--- scripts/helm-release.sh
+++ scripts/helm-release.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-helm package --version "$1" charts/helm-test
-mv "helm-test-$1.tgz" docs/
+helm package --version "$TAG" charts/helm-test
+mv "helm-test-$TAG.tgz" docs/
 helm repo index docs --url https://cedriclam.github.io/helm-test/
 git add --all .
-git commit -am "release $1"
-git tag -f $1
-git push  --tags origin master
+git commit -am "release $TAG"
+git tag -f $TAG
+git push -f --tags origin master
